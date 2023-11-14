32b6db91c95a40a8f03859c2e05cfb878d0f753d
Sunghwan Bang <sunghwan2789@gmail.com>
2023-11-14 22:12:21 +0900

    repro

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,51 @@
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using HotChocolate.Transport.Http;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGraphQLServer()
+    .AddQueryType(_ => _.Field("foo").Resolve(1))
+    .AddSubscriptionType<Subscription>()
+    ;
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGraphQL();
+
+_ = DoSubscription(app.Lifetime.ApplicationStopped);
 
 app.Run();
+
+async Task DoSubscription(CancellationToken stopped) {
+    var httpClient = new HttpClient();
+    using var client = GraphQLHttpClient.Create(httpClient, true);
+
+    while (!stopped.IsCancellationRequested) {
+        try {
+            var request = new GraphQLHttpRequest("""subscription S { bar }""", new Uri("http://localhost:5241/graphql"));
+            using var result = await client.SendAsync(request, stopped);
+            await foreach (var response in result.ReadAsResultStreamAsync(stopped)) {
+                Console.WriteLine(JsonSerializer.Serialize(response.Data));
+            }
+        } catch (Exception ex) {
+            Console.WriteLine("retry... " + ex);
+            await Task.Delay(1000, stopped);
+        }
+    }
+}
+
+public class Subscription {
+
+    [Subscribe(With = nameof(BarStream))]
+    public int Bar([EventMessage] int message) => message;
+
+    private static async IAsyncEnumerable<int> BarStream([EnumeratorCancellation] CancellationToken cancellationToken) {
+        var i = 0; 
+        while (!cancellationToken.IsCancellationRequested) {
+            yield return i++;
+            await Task.Delay(1000, cancellationToken);
+        }
+    }
+}
+
